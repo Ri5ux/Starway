c22baa2746cf66c2bc95e049a154e102b7f5e28b
Ri5ux <Ri5ux@users.noreply.github.com>
2019-06-20 08:34:33 -0500

    WIP Fix for sphere models.

--- src/main/java/com/arisux/starway/ModelSphere.java
+++ src/main/java/com/arisux/starway/ModelSphere.java
@@ -8,6 +8,7 @@ import com.asx.mdx.lib.client.util.OpenGL;
 import com.asx.mdx.lib.client.util.Vertex;
 import com.asx.mdx.lib.client.util.models.Model;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 
@@ -17,6 +18,7 @@ public class ModelSphere extends Model
     private Color       color;
     private float       scale;
     public boolean      cull        = true;
+    public boolean      invert      = false;
 
     private Vertex      vX          = new Vertex(1.0F, 0.0F, 0.0F).normalize();
     private Vertex      vY          = new Vertex(0.0F, 1.0F, 0.0F).normalize();
@@ -60,18 +62,37 @@ public class ModelSphere extends Model
         float nX = v1.x * 1.1F;
         float nY = v1.y * (v1 == vY || v1 == vY1 || v1 == vY3 ? -1.1F : 1.1F);
         float nZ = v1.z * 1.1F;
+        
+        Draw.bindTexture(Starway.resources().SKY_SUN);
 
-        Draw.buffer().begin(GL11.GL_TRIANGLE_FAN, DefaultVertexFormats.POSITION_TEX_COLOR);
-        Draw.buffer().pos(v1.x, v1.y, v1.z).normal(nX, nY, nZ).color(this.color.r, this.color.g, this.color.b, this.color.a).endVertex();
-        Draw.buffer().pos(v2.x, v2.y, v2.z).normal(nX, nY, nZ).color(this.color.r, this.color.g, this.color.b, this.color.a).endVertex();
-        Draw.buffer().pos(v3.x, v3.y, v3.z).normal(nX, nY, nZ).color(this.color.r, this.color.g, this.color.b, this.color.a).endVertex();
-
-        if (cull)
+//        if (!invert)
         {
-            Draw.buffer().pos(v3.x, v3.y, v3.z).normal(nX, nY, nZ).color(this.color.r, this.color.g, this.color.b, this.color.a).endVertex();
-            Draw.buffer().pos(v2.x, v2.y, v2.z).normal(nX, nY, nZ).color(this.color.r, this.color.g, this.color.b, this.color.a).endVertex();
-            Draw.buffer().pos(v1.x, v1.y, v1.z).normal(nX, nY, nZ).color(this.color.r, this.color.g, this.color.b, this.color.a).endVertex();
+            Draw.buffer().begin(GL11.GL_TRIANGLE_FAN, DefaultVertexFormats.POSITION_TEX_NORMAL);
+            Draw.buffer().pos(v3.x, v3.y, v3.z).normal(nX, nY, nZ).color(this.color.r, this.color.g, this.color.b, this.color.a).endVertex();// flipped these
+            Draw.buffer().pos(v2.x, v2.y, v2.z).normal(nX, nY, nZ).color(this.color.r, this.color.g, this.color.b, this.color.a).endVertex();// flipped these
+            Draw.buffer().pos(v1.x, v1.y, v1.z).normal(nX, nY, nZ).color(this.color.r, this.color.g, this.color.b, this.color.a).endVertex();// flipped these
+
+            if (cull)
+            {
+//                Draw.buffer().pos(v1.x, v1.y, v1.z).normal(nX, nY, nZ).color(this.color.r, this.color.g, this.color.b, this.color.a).endVertex();// flipped these
+//                Draw.buffer().pos(v2.x, v2.y, v2.z).normal(nX, nY, nZ).color(this.color.r, this.color.g, this.color.b, this.color.a).endVertex();// flipped these
+//                Draw.buffer().pos(v3.x, v3.y, v3.z).normal(nX, nY, nZ).color(this.color.r, this.color.g, this.color.b, this.color.a).endVertex();// flipped these
+            }
         }
+//        else
+//        {
+//            Draw.buffer().begin(GL11.GL_TRIANGLE_FAN, DefaultVertexFormats.POSITION_TEX_COLOR);
+//            Draw.buffer().pos(v1.x, v1.y, v1.z).normal(nX, nY, nZ).color(this.color.r, this.color.g, this.color.b, this.color.a).endVertex();// flipped these
+//            Draw.buffer().pos(v2.x, v2.y, v2.z).normal(nX, nY, nZ).color(this.color.r, this.color.g, this.color.b, this.color.a).endVertex();// flipped these
+//            Draw.buffer().pos(v3.x, v3.y, v3.z).normal(nX, nY, nZ).color(this.color.r, this.color.g, this.color.b, this.color.a).endVertex();// flipped these
+//
+//            if (cull)
+//            {
+//                Draw.buffer().pos(v3.x, v3.y, v3.z).normal(nX, nY, nZ).color(this.color.r, this.color.g, this.color.b, this.color.a).endVertex();// flipped these
+//                Draw.buffer().pos(v2.x, v2.y, v2.z).normal(nX, nY, nZ).color(this.color.r, this.color.g, this.color.b, this.color.a).endVertex();// flipped these
+//                Draw.buffer().pos(v1.x, v1.y, v1.z).normal(nX, nY, nZ).color(this.color.r, this.color.g, this.color.b, this.color.a).endVertex();// flipped these
+//            }
+//        }
 
         tessellator.draw();
     }
@@ -91,7 +112,7 @@ public class ModelSphere extends Model
     {
         OpenGL.pushMatrix();
         {
-            OpenGL.enableRescaleNormal();
+//            OpenGL.enableRescaleNormal();
             OpenGL.scale(scale, scale, scale);
             OpenGL.rotate(180F, 0F, 0F, 1F);
 
@@ -101,50 +122,50 @@ public class ModelSphere extends Model
                 {
                     OpenGL.rotate(quadrant * 90, 0F, 1F, 0F);
 
-                    addTri(vY, vY1, vY3);
-                    addTri(vY1, vYZ, vY2);
-                    addTri(vY3, vY2, vXY);
-                    addTri(vY1, vY2, vY3);
+//                    addTri(vY, vY1, vY3);
+//                    addTri(vY1, vYZ, vY2);
+//                    addTri(vY3, vY2, vXY);
+//                    addTri(vY1, vY2, vY3);
+//
+//                    addTri(vX, vX1, vX3);
+//                    addTri(vX1, vXY, vX2);
+//                    addTri(vX3, vX2, vXZ);
+//                    addTri(vX1, vX2, vX3);
+//
+//                    addTri(vZ, vZ1, vZ3);
+//                    addTri(vZ1, vXZ, vZ2);
+//                    addTri(vZ3, vZ2, vYZ);
+//                    addTri(vZ1, vZ2, vZ3);
+//
+//                    addTri(vYZ, vZ2, vY2);
+//                    addTri(vXY, vY2, vX2);
+//                    addTri(vXZ, vX2, vZ2);
+//                    addTri(vX2, vY2, vZ2);
 
-                    addTri(vX, vX1, vX3);
-                    addTri(vX1, vXY, vX2);
-                    addTri(vX3, vX2, vXZ);
-                    addTri(vX1, vX2, vX3);
-
-                    addTri(vZ, vZ1, vZ3);
-                    addTri(vZ1, vXZ, vZ2);
-                    addTri(vZ3, vZ2, vYZ);
-                    addTri(vZ1, vZ2, vZ3);
-
-                    addTri(vYZ, vZ2, vY2);
-                    addTri(vXY, vY2, vX2);
-                    addTri(vXZ, vX2, vZ2);
-                    addTri(vX2, vY2, vZ2);
-
-                    addTri(hvY, hvY1, hvY3);
+//                    addTri(hvY, hvY1, hvY3);
                     addTri(hvY1, hvYZ, hvY2);
-                    addTri(hvY3, hvY2, hvXY);
-                    addTri(hvY1, hvY2, hvY3);
-
-                    addTri(hvX, hvX1, hvX3);
-                    addTri(hvX1, hvXY, hvX2);
-                    addTri(hvX3, hvX2, hvXZ);
-                    addTri(hvX1, hvX2, hvX3);
-
-                    addTri(hvZ, hvZ1, hvZ3);
-                    addTri(hvZ1, hvXZ, hvZ2);
-                    addTri(hvZ3, hvZ2, hvYZ);
-                    addTri(hvZ1, hvZ2, hvZ3);
-
-                    addTri(hvYZ, hvZ2, hvY2);
-                    addTri(hvXY, hvY2, hvX2);
-                    addTri(hvXZ, hvX2, hvZ2);
-                    addTri(hvX2, hvY2, hvZ2);
+//                    addTri(hvY3, hvY2, hvXY);
+//                    addTri(hvY1, hvY2, hvY3);
+//
+//                    addTri(hvX, hvX1, hvX3);
+//                    addTri(hvX1, hvXY, hvX2);
+//                    addTri(hvX3, hvX2, hvXZ);
+//                    addTri(hvX1, hvX2, hvX3);
+//
+//                    addTri(hvZ, hvZ1, hvZ3);
+//                    addTri(hvZ1, hvXZ, hvZ2);
+//                    addTri(hvZ3, hvZ2, hvYZ);
+//                    addTri(hvZ1, hvZ2, hvZ3);
+//
+//                    addTri(hvYZ, hvZ2, hvY2);
+//                    addTri(hvXY, hvY2, hvX2);
+//                    addTri(hvXZ, hvX2, hvZ2);
+//                    addTri(hvX2, hvY2, hvZ2);
 
                 }
                 OpenGL.popMatrix();
             }
-            OpenGL.disableRescaleNormal();
+//            OpenGL.disableRescaleNormal();
         }
         OpenGL.popMatrix();
     }
